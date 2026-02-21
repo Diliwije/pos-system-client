aa46c25af74a648e3bc7cd0e5c0a296132db7a64
Dilshan <dilshanwijerathna426@gmail.com>
2026-02-21 21:10:24 +0530

    Remove starter template and configure initial routes
    - Replace default Angular content in `src/app/app.html` with `<router-outlet/>`.
    - Configure routes for `Login` and `Notfound` components in `src/app/app.routes.ts`.
    - Add default redirect from root to `/login`.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {Login} from './pages/login/login';
+import {Notfound} from './pages/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: Login },
+  { path: '**', component: Notfound }
+
+];
